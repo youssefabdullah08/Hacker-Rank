9e68cf24f134c39e3bd9f6a2cf1ed6c274d7ffef
Youssef Abdulluh <151579855+youssefabdullah08@users.noreply.github.com>
2024-08-10 23:35:28 +0300

    tofixed

---
+++ Plus Minus/code.js
@@ -0,0 +1,63 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+    let positiveCount = 0;
+    let negativeCount = 0;
+    let zeroCount = 0;
+    const totalElements = arr.length;
+
+    arr.forEach(num => {
+        if (num > 0) {
+            positiveCount++;
+        } else if (num < 0) {
+            negativeCount++;
+        } else {
+            zeroCount++;
+        }
+    });
+
+    // Calculate ratios
+    const positiveRatio = positiveCount / totalElements;
+    const negativeRatio = negativeCount / totalElements;
+    const zeroRatio = zeroCount / totalElements;
+
+    // Print with 6 decimal places
+    console.log(positiveRatio.toFixed(6));
+    console.log(negativeRatio.toFixed(6));
+    console.log(zeroRatio.toFixed(6));
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
